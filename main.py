610749e1af7d8ffa29e61beacc78cb2ffeb22679
Marc Hibbins <marchibbins@gmail.com>
2012-10-25 16:28:15 +0100

    Flickr API and demo call

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+import flickr_api
 import jinja2
 import os
+import settings
 import webapp2
 
 
 class Index(webapp2.RequestHandler):
     def get(self):
+        photoset = flickr_api.Photoset(id=settings.PHOTOSET_ID)
+        data = {
+            'photos': flickr_api.Photoset.getPhotos(photoset)
+        }
         template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(data))
 
 
 def handle_404(request, response, exception):
     template = jinja_environment.get_template('404.html')
     response.write(template.render())
     response.set_status(404)
+
+flickr_api.set_keys(
+    api_key=settings.API_KEY,
+    api_secret=settings.API_SECRET
+)
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(
