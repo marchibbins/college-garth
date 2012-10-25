e09274f84c81d9547440c7187997826d20275937
Marc Hibbins <marchibbins@gmail.com>
2012-10-25 13:41:32 +0100

    Basic templating and static files

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import jinja2
+import os
 import webapp2
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), 'templates')
+    )
+)
 
 class Home(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello World')
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([('/', Home)], debug=True)
