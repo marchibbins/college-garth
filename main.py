8e99362db2ae46e3d9d38ebf0909a909d7492161
Marc Hibbins <marchibbins@gmail.com>
2012-10-25 15:21:21 +0100

    Code clean-up

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import jinja2
 import os
 import webapp2
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(
-        os.path.join(os.path.dirname(__file__), 'templates')
-    )
-)
 
 class Index(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render())
 
+
 def handle_404(request, response, exception):
     template = jinja_environment.get_template('404.html')
     response.write(template.render())
     response.set_status(404)
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), 'templates')
+    )
+)
 
 app = webapp2.WSGIApplication([('/', Index)], debug=True)
 app.error_handlers[404] = handle_404
