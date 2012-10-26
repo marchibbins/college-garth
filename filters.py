f127c86b9577b6f5577747b7a2fc835c127f642a
Marc Hibbins <marchibbins@gmail.com>
2012-10-26 16:09:45 +0100

    Size option for image filter

--- filters.py
+++ filters.py
@@ -2,8 +2,9 @@
 
 
 # Construct image URL from Photo object
-def flickr_src(photo):
-    return 'http://farm%(farm)s.staticflickr.com/%(server)s/%(id)s_%(secret)s_m.jpg' % photo
+def flickr_src(photo, size='m'):
+    photo['size'] = size
+    return 'http://farm%(farm)s.staticflickr.com/%(server)s/%(id)s_%(secret)s_%(size)s.jpg' % photo
 
 
 jinja_filters = {
