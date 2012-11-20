7af87c5906d36172ef67a13b1d384178ac4130a4
Marc Hibbins <marchibbins@gmail.com>
2012-11-20 11:35:55 +0000

    Flickr filter update

--- filters.py
+++ filters.py
@@ -2,9 +2,9 @@
 
 
 # Construct image URL from Photo object
-def flickr_src(photo, size='m'):
-    photo['size'] = size
-    return 'http://farm%(farm)s.staticflickr.com/%(server)s/%(id)s_%(secret)s_%(size)s.jpg' % photo
+def flickr_src(photo, size=None):
+    photo['size'] = '_%s' % size if size else ''
+    return 'http://farm%(farm)s.staticflickr.com/%(server)s/%(id)s_%(secret)s%(size)s.jpg' % photo
 
 
 jinja_filters = {
